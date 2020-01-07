7d6489e052037530ce508b6293c698fe80d5ffca
Islomzoda <Islomzoda17@gmail.com>
2020-01-07 07:56:48 +0200

    Добавили Лимит кэшбеку

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
-const regularPurchase = 2000;
+const regularPurchase = 3000;
 const regularPurchasePercent = 0.01;
-const cashback = regularPurchase * regularPurchasePercent;
+const cashbackLimit = 3000;
+let cashback = regularPurchase * regularPurchasePercent;
+if (cashback > cashbackLimit) {
+    cashback = cashbackLimit
+}
 console.log(cashback);
